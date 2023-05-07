2f1e511abfb95376ad6867f6e98f254c3571e8e8
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 19:33:31 +1000

    Created skelton for the game object

---
+++ GameObject.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "GameObject.h"
+
+GameObject::GameObject()
+{
+}
+
+GameObject::~GameObject()
+{
+}
+
+void GameObject::Initialize()
+{
+}
+
+void GameObject::Update()
+{
+}
+
+void GameObject::Draw()
+{
+}
+
+void GameObject::Destroy()
+{
+}
+
+void GameObject::SetParent(GameObject* go)
+{
+}
+
+void GameObject::AddChild(GameObject* child)
+{
+}
+
+void GameObject::RemoveChild(GameObject* child)
+{
+}
--- GameObject.h
+++ GameObject.h
@@ -9,8 +9,33 @@ public:
 	GameObject();
 	~GameObject();
 
+	void Initialize();
+	void Update();
+	void Draw();
+	void Destroy();
 
+	void SetParent(GameObject* go);
+	void AddChild(GameObject* child);
+	void RemoveChild(GameObject* child);
+
+	std::string ObjectName;
+
+	bool IsDrawable;
+	bool IsActive;
+
+	GameObject* Parent;
+	
 private:
+	void Deinitialize();
+
+	void UpdateTransform();
+
+	Vector2 _LocalPosition;
+	Vector2 _GlobalPosition;
+	float scale;
+	float rotation;
+
+	std::vector<GameObject*> _Children;
 
 };
 
