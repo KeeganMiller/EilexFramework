f1b35499aef037cfcc4a6fa94646d822f592eb53
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 20:14:05 +1000

    Added constructors

--- GameState.cpp
+++ GameState.cpp
@@ -1,6 +1,34 @@
 #include "pch.h"
 #include "GameState.h"
+#include "AssetManager.h"
+
+GameState::GameState(std::string stateName)
+{
+	_StateName = stateName;
+	_StateAssets = new AssetManager(this);
+}
+
+GameState::~GameState()
+{
+	delete _StateAssets;
+}
 
 void GameState::Initialize()
+{
+}
+
+void GameState::Start()
+{
+}
+
+void GameState::Update()
+{
+}
+
+void GameState::Draw()
+{
+}
+
+void GameState::Deinitialize()
 {
 }
