c27bfcd05f73238d81bbcde6f6ce397a974fa078
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 12:46:02 +1000

    Initial Commit

---
+++ EilexFramework.cpp
@@ -0,0 +1,10 @@
+// EilexFramework.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnEilexFramework()
+{
+}
