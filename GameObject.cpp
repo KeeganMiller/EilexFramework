3b0b497bb0e50b72970e2e31d64de0c2a3ba4b76
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 19:43:59 +1000

    Created the parening methods

--- GameObject.cpp
+++ GameObject.cpp
@@ -18,28 +18,60 @@ GameObject::~GameObject()
 
 void GameObject::Initialize()
 {
+	if (!IsActive)
+		return;
 }
 
 void GameObject::Update()
 {
+	if (!IsActive)
+		return;
 }
 
 void GameObject::Draw()
 {
+	if (!IsActive)
+		return;
 }
 
 void GameObject::Destroy()
 {
+	if (!IsActive)
+		return;
 }
 
 void GameObject::SetParent(GameObject* go)
 {
+	if (this->Parent != nullptr)
+	{
+		this->Parent->RemoveChild(this);
+	}
+
+	this->Parent = go;
+
+	this->Parent->AddChild(this);
+
 }
 
 void GameObject::AddChild(GameObject* child)
 {
+	_Children.push_back(child);
 }
 
 void GameObject::RemoveChild(GameObject* child)
 {
+	int index = -1;
+	for (int i = 0; i < _Children.size(); ++i)
+	{
+		if (child == _Children[i])
+		{
+			index = i;
+			break;
+		}
+	}
+
+	if (index > -1)
+	{
+		_Children.erase(_Children.begin() + index);
+	}
 }
