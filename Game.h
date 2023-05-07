a1de1927e5422848199bdcba9344a9fd59ea023a
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 19:24:04 +1000

    Misc

--- Game.h
+++ Game.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include "framework.h"
+#include "raylib.h"
 
 
 class Game
@@ -9,25 +10,33 @@ public:
 	Game();
 	~Game();
 
-	void Initialize();
-	void Start();
-	void Update();
-	void Draw();
-	void Deintialize();
+	static void Initialize();
+	static void Start();
+	static void Update();
+	static void Draw();
+	static void Deintialize();
 
 	static void CreateWindow(int windowWidth, int windowHeight, std::string windowTitle, bool fullscreen = false);
 	static void ToggleWindowFullscreen(bool fullscreen);
 
-	static int WindowWidth;
-	static int WindowHeight;
-	static std::string WindowTitle;
-	static bool IsFullscreen;
+	
 
 	static void CloseGame();
 
 	inline bool IsGameRunning() { return _IsRunning; }
+
+	// Size of the window
+	static int WindowWidth;
+	static int WindowHeight;
+
+	static std::string WindowTitle;					// Name of the window
+	
+	// Window Settings
+	static bool IsFullscreen;
+
+	static Color ClearColor;						// Reference to the color that renders when the drawing clears
 	
 private:
-	static bool _IsRunning;
+	static bool _IsRunning;							// Reference to if the game is running
 };
 
