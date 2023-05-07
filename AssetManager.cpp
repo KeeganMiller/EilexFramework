a8a469bd9bd3fb02262e530e8beb681027ad82cd
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 20:06:22 +1000

    Setup skelton code

--- AssetManager.cpp
+++ AssetManager.cpp
@@ -1,2 +1,60 @@
 #include "pch.h"
 #include "AssetManager.h"
+#include "GameObject.h"
+
+AssetManager::AssetManager(GameState* state)
+{
+	_OwningState = state;
+}
+
+AssetManager::~AssetManager()
+{
+	for (auto* go : _ObjectsInScene)
+	{
+		delete go;
+	}
+}
+
+void AssetManager::Initialize()
+{
+}
+
+void AssetManager::Start()
+{
+	for (auto* go : _ObjectsInScene)
+	{
+		go->Start();
+	}
+}
+
+void AssetManager::Update()
+{
+	for (auto* go : _ObjectsInScene)
+	{
+		go->Update();
+	}
+}
+
+void AssetManager::Draw()
+{
+	for (auto* go : _ObjectsInScene)
+	{
+		go->Draw();
+	}
+}
+
+void AssetManager::Deinitialize()
+{
+	for (auto* go : _ObjectsInScene)
+	{
+		go->Destroy();
+	}
+}
+
+void AssetManager::AddGameObject(GameObject* go)
+{
+}
+
+void AssetManager::RemoveGameObject(GameObject* go)
+{
+}
--- AssetManager.h
+++ AssetManager.h
@@ -1,14 +1,27 @@
 #pragma once
 #include <vector>
 #include "GameState.h"
+
+class GameObject;
+
 class AssetManager
 {
 public:
 	AssetManager(GameState* state);
 	~AssetManager();
 
+	void Initialize();
+	void Start();
+	void Update();
+	void Draw();
+	void Deinitialize();
+
+	void AddGameObject(GameObject* go);
+	void RemoveGameObject(GameObject* go);
+
 
 private:
 	GameState* _OwningState;
+	std::vector<GameObject*> _ObjectsInScene;
 
 };
