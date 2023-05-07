646def6a9abd54f909a66591ffb8bf26976cf8dd
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 12:50:36 +1000

    Create initial skelton

---
+++ Game.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+#include "Game.h"
+
+int Game::WindowHeight = 0;
+int Game::WindowWidth = 0;
+std::string Game::WindowTitle = "Raylib - Eilex Framework";
+bool Game::IsWindowFullscreen = false;
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Initialize()
+{
+}
+
+void Game::Start()
+{
+}
+
+void Game::Update()
+{
+}
+
+void Game::Draw()
+{
+}
+
+void Game::Deintialize()
+{
+}
+
+void Game::CreateWindow(int windowWidth, int windowHeight, std::string windowTitle, bool fullscreen)
+{
+}
--- Game.h
+++ Game.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <string>
 class Game
 {
 public:
+	Game();
+	~Game();
 
+	void Initialize();
+	void Start();
+	void Update();
+	void Draw();
+	void Deintialize();
+
+	static void CreateWindow(int windowWidth, int windowHeight, std::string windowTitle, bool fullscreen = false);
+
+	static int WindowWidth;
+	static int WindowHeight;
+	static std::string WindowTitle;
+	static bool IsWindowFullscreen;
 private:
 
 };
