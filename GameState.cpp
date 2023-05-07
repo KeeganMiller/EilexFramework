01af11115126c32233990e34cdd1d2b112d11d6c
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-07 20:12:28 +1000

    Created skeleton code for the game state

--- GameState.cpp
+++ GameState.cpp
@@ -1,2 +1,6 @@
 #include "pch.h"
 #include "GameState.h"
+
+void GameState::Initialize()
+{
+}
--- GameState.h
+++ GameState.h
@@ -1,7 +1,26 @@
 #pragma once
+#include <string>
+
+
+class AssetManager;
+
 class GameState
 {
 public:
+	GameState(std::string stateName = "Game State");
+	~GameState();
 
+	virtual void Initialize();
+	virtual void Start();
+	virtual void Update();
+	virtual void Draw();
+	virtual void Deinitialize();
+
+	inline std::string GetStateName() { return _StateName; }
+	inline AssetManager* GetAssetManager() { return _StateAssets; }
+
+private:
+	std::string _StateName;
+	AssetManager* _StateAssets;
 };
 
