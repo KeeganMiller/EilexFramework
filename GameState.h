b289300b7f1762336b04201e88b357c13d09eda9
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-08 20:29:51 +1000

    Setup game state

--- GameState.h
+++ GameState.h
@@ -10,14 +10,15 @@ public:
 	GameState(std::string stateName = "Game State");
 	~GameState();
 
+	// Update functions
 	virtual void Initialize();
 	virtual void Start();
 	virtual void Update();
 	virtual void Draw();
 	virtual void Deinitialize();
 
-	inline std::string GetStateName() { return _StateName; }
-	inline AssetManager* GetAssetManager() { return _StateAssets; }
+	inline std::string GetStateName() { return _StateName; }			// Getter for the name of the state
+	inline AssetManager* GetAssetManager() { return _StateAssets; }		// Get reference to the asset manager
 
 private:
 	std::string _StateName;
