670a1056d9bfcf28333a4a223e8c21e1b1c8cc8b
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-08 21:34:15 +1000

    Created the initial sprite class

---
+++ Sprite.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "Sprite.h"
+#include "TextureDetails.h"
+
+Sprite::Sprite(std::string objectName)
+{
+	IsDrawable = false;
+}
+
+Sprite::Sprite(TextureDetails* texture, std::string objectName)
+{
+	_TextureInfo = texture;
+	if (_TextureInfo->IsValid)
+	{
+		IsDrawable = true;
+	}
+}
+
+Sprite::~Sprite()
+{
+	delete _TextureInfo;
+}
+
+void Sprite::Draw()
+{
+	if (IsDrawable)
+	{
+		DrawTextureEx(_TextureInfo->TextureAsset, +_GlobalPosition, _Rotation, _Scale, WHITE);
+	}
+}
--- Sprite.h
+++ Sprite.h
@@ -1,12 +1,22 @@
 #pragma once
 #include "GameObject.h"
+
+class TextureDetails;
+
 class Sprite :
     public GameObject
 {
 public:
-    Sprite(std::string objectName = "Sprite") : GameObject(objectName)
-    {}
+    Sprite(std::string objectName = "Sprite");
+    Sprite(TextureDetails* texture, std::string objectName = "Sprite") : GameObject(objectName);
 
     ~Sprite();
+
+    void Draw() override;
+
+protected:
+    TextureDetails* _TextureInfo;
+
+
 };
 
